690e58c51196325fab181f42ff8a03baa31efea7
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-20 09:42:56 -0600

    update file

---
+++ Flare Sharp/Memory/FlameSDK/Item.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flare_Sharp.Memory.FlameSDK
+{
+    public class Item : SDKObj
+    {
+        public Item(ulong addr) : base(addr) { }
+    }
+}
